a7e6d2586e23c6d92e6b84f06b019c23bf95592f
Georg9741 <96758662+Georg9741@users.noreply.github.com>
2025-04-01 10:56:52 +0200

    Update install.sh

--- archlinux/install.sh
+++ archlinux/install.sh
@@ -69,9 +69,7 @@ done
 # passwordhash=`openssl passwd -1 $password`
 
 # Partitioning
-echo
-echo "Partitioning..."
-echo
+echo "[Partitioning]"
 wipefs -a /dev/sda*
 gdisk /dev/sda <<EOF
 o
@@ -94,14 +92,10 @@ n
 w
 Y
 EOF
-echo
-echo "...finished"
-echo
+echo "[Partitioning finished]"
 
 # Format partitions
-echo
-echo "Format partitions..."
-echo
+echo "[Format partitions]"
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 cryptsetup -q luksFormat /dev/sda3 <<EOF
@@ -110,81 +104,53 @@ EOF
 cryptsetup open /dev/sda3 luks_lvm <<EOF
 $CRYPTPASSWD
 EOF
-echo
-echo "...finished"
-echo
+echo "[Partitions formatted]"
 
 # LVM Setup
-echo
-echo "LVM Setup..."
-echo
+echo "[LVM Setup]"
 pvcreate /dev/mapper/luks_lvm
 vgcreate arch /dev/mapper/luks_lvm
 lvcreate arch -n swap -L 32GB -C y
 lvcreate arch -n root -L 64GB
 lvcreate arch -n home -l +100%FREE
-echo
-echo "...finished"
-echo
+echo "[LVM Setup finished]"
 
 # Format LVM partitions
-echo
-echo "Format LVM partitions..."
-echo
+echo "[Format LVM partitions]"
 mkswap /dev/mapper/arch-swap
 mkfs.ext4 /dev/mapper/arch-root -L root
 mkfs.ext4 /dev/mapper/arch-home -L home
-echo
-echo "...finished"
-echo
+echo "[LVM partitions formatted]"
 
 # Mount filesystems
-echo
-echo "Mount filesystems..."
-echo
+echo "[Mount filesystems]"
 mount /dev/mapper/arch-root /mnt
 mount /dev/mapper/arch-home /mnt/home --mkdir
 mount /dev/sda2 /mnt/boot --mkdir
 mount /dev/sda1 /mnt/boot/efi --mkdir
 swapon /dev/mapper/arch-swap
-echo
-echo "...finished"
-echo
+echo "[Filesystems mounted]"
 
 # Generate mirror list
-echo
-echo "Generate mirror list..."
-echo
+echo "[Generate mirror list]"
 reflector -l 10 -p https -c DE --sort rate --save /etc/pacman.d/mirrorlist
-echo
-echo "...finished"
-echo
+echo "[Mirror list generated]"
 
 # Install base system
-echo
-echo "Install packages..."
-echo
+echo "[Install packages]"
 PACKAGES='base linux linux-headers linux-firmware base-devel efibootmgr git grub lvm2 nano networkmanager os-prober linux-zen linux-zen-headers intel-ucode plasma openssh kitty fastfetch mesa intel-media-driver'
 pacstrap -K /mnt $PACKAGES
 # Selection extra kernels: pacstrap -K /mnt linux-zen linux-zen-headers | pacstrap -K /mnt linux-lts linux-lts-headers
 # Selection microcode: pacstrap -K /mnt amd-ucode | pacstrap -K /mnt intel-ucode (lscpu, automatic with vendor id)
-echo
-echo "...finished"
-echo
+echo "[Packages installed]"
 
 # Generate fstab
-echo
-echo "Generate fstab..."
-echo
+echo "[Generate fstab]"
 genfstab -U /mnt >> /mnt/etc/fstab
-echo
-echo "...finished"
-echo
+echo "[fstab generated]"
 
 # Enter chroot
-echo
-echo "Enter chroot..."
-echo
+echo "[Enter chroot]"
 arch-chroot /mnt <<EOF
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 sed -i 's/#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/;s/#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/;s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
@@ -210,15 +176,10 @@ systemctl enable NetworkManager
 systemctl enable sddm
 systemctl enable sshd
 EOF
-echo
-echo "...finished"
-echo
+echo "[Exit chroot]"
 clearscreen
 
-
-echo
-echo "Partitioning"
-echo
+echo "[Partitioning]"
 sda1size=$(($(blockdev --getsize64 /dev/sda1)/1048576))
 sda2size=$(($(blockdev --getsize64 /dev/sda2)/1048576))
 sda3size=$(($(blockdev --getsize64 /dev/sda3)/1073741824))
@@ -226,20 +187,17 @@ echo "sda1: EFI system partition ($sda1size MB)"
 echo "sda2: BIOS boot partition ($sda2size MB)"
 echo "sda3: Linux LUKS ($sda3size GB)"
 echo
-echo "Format partitions"
-echo
+echo "[Partitions formatted]"
 echo "sda1: fat"
 echo "sda2: ext4"
 echo "sda3: luks"
 echo
-echo "LVM Setup"
-echo
+echo "[LVM Setup]"
 echo "swap: 32 GB"
 echo "root: 64 GB"
 echo "home: 'todo: calculate +100%FREE' GB"
 echo
-echo "Enter chroot"
-echo
+echo "[Chroot]"
 echo "root password set"
 echo "user georg created"
 echo "user added to group wheel"
@@ -251,6 +209,5 @@ clearscreen
 # Unmount and reboot
 umount -R /mnt
 swapoff -a
-clear
 read -p "Press enter to reboot"
 reboot
